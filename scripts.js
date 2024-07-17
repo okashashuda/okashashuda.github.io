e3d237acf845c52da84b72fbf2b6c18cc50ea38b
Okasha Huda <okashahuda@hotmail.com>
2024-07-17 02:12:30 -0700

    created scripts.js and completed typewriter animation for introduction

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typewriter", {
+    strings: ["student", "data enthusiast", "would-be software engineer", "sports fanatic"],
+    typeSpeed: 75,
+    backSpeed: 100,
+    loop: true
+})
