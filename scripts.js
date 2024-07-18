ba5a9e220eb9fdc4748e1448750a54f067eb8634
Okasha Huda <okashahuda@hotmail.com>
2024-07-17 18:45:59 -0700

    adding a mouse scroll animation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".typewriter", {
-    strings: ["student", "data enthusiast", "would-be software engineer", "sports fanatic"],
+    strings: ["student", "data science enthusiast", "would-be software engineer", "sports fanatic"],
     typeSpeed: 75,
-    backSpeed: 100,
+    backSpeed: 80,
     loop: true
 })
