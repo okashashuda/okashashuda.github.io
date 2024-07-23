2e1a29b4c4a7f2c721a5e9fd3dac245a446a909c
Okasha Huda <okashahuda@hotmail.com>
2024-07-22 19:56:29 -0700

    fixed nav bar links and added active scroll effect

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 var typed = new Typed(".typewriter", {
-    strings: ["student", "data science enthusiast", "would-be software engineer", "sports fanatic"],
+    strings: ["student", "data science enthusiast", "wanna-be software engineer", "sports fanatic"],
     typeSpeed: 75,
     backSpeed: 80,
     loop: true
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    function changeActiveLink() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+    }
+
+    changeActiveLink();
+    window.addEventListener("scroll", changeActiveLink);
+});
+
