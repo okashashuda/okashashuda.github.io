1cdf280deb0bd4bb47f1be8b145fd52631e01ee2
Okasha Huda <okashahuda@hotmail.com>
2024-07-29 00:35:25 -0700

    created a dropdown menu for responsiveness on mobile screens

--- scripts.js
+++ scripts.js
@@ -6,21 +6,45 @@ var typed = new Typed(".typewriter", {
     loop: true
 })
 
-// Navigation Bar Active Scroll Animation
-document.addEventListener("DOMContentLoaded", function() {
+// Navigation Bar + Dropdown Menu Active Scroll Animation
+document.addEventListener("DOMContentLoaded", function () {
     const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav ul li a");
+    const navLinks = document.querySelectorAll(".menu-items ul li a, .dropdown-menu li a");
 
     function changeActiveLink() {
         let index = sections.length;
 
-        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+        while (--index && window.scrollY + 50 < sections[index].offsetTop) { }
 
         navLinks.forEach((link) => link.classList.remove("active"));
-        navLinks[index].classList.add("active");
+        document.querySelectorAll(`.menu-items ul li a[href="#${sections[index].id}"], .dropdown-menu li a[href="#${sections[index].id}"]`).forEach((link) => link.classList.add("active"));
     }
 
     changeActiveLink();
     window.addEventListener("scroll", changeActiveLink);
 });
 
+// Dropdown Menu Open/Close Animation
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector(".menu-icon");
+    const menuIconLogo = menuIcon.querySelector("i");
+    const dropdown = document.querySelector(".dropdown-menu");
+    const dropdownLinks = dropdown.querySelectorAll("a");
+
+    menuIcon.addEventListener("click", function() {
+        dropdown.classList.toggle("open");
+        const isOpen = dropdown.classList.contains("open");
+
+        menuIconLogo.className = isOpen
+            ? "fa fa-close fa-xl"
+            : "fa fa-bars fa-xl";
+    });
+
+    // auto-close when a link on the dropdown menu has been clicked
+    dropdownLinks.forEach(link => {
+        link.addEventListener("click", function() {
+            dropdown.classList.remove("open");
+            menuIconLogo.className = "fa fa-bars fa-xl"; //reset icon to menu
+        });
+    });
+});
